93fbeec8f51824446eb2cd86b76abd1a0fed8ac6
Shaan Varia <shaanvaria@Shaans-MacBook-Air.local>
2013-11-17 18:35:27 -0500

    fixed bugs

--- printsubs.py
+++ printsubs.py
@@ -5,11 +5,16 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.meteor
 sreddits = db.subreddits
+smissions = db.submissions
 
 def printsubs():
   slist = scrape.read_subreddits()
   sreddits.insert(slist)
-  #print slist
+
+def printsubm():
+  slist = scrape.read_submissions()
+  smissions.insert(slist)
 
 
 printsubs()
+printsubm()
